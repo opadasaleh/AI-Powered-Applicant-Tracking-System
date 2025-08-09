967f83fd090c0e21bef469c810040a2b0112f1c3
opada <opadasadleh@gmail.com>
2025-08-09 22:30:49 +0300

    continue resumecard

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,20 @@
 import { Link } from "react-router"
+import ScoreCircle from "./ScoreCircle"
 
-function ResumeCard({resume :{resume:Resume}}) {
+function ResumeCard({ resume: {id, companyName, jobTitle, feedback} } :{resume:Resume}) {
   return (
-    <Link to="`/resume/${resume.id}`}">ResumeCard</Link>
+      <Link to="`/resume/${resume.id}`}" className="resume-card animate-in fade-in duration-1000">
+         <div className="resume-card-header">
+          <div className="flex flex-col gap-2">
+              <h2 className="!text-black font-bold break-words">{companyName}</h2>
+              <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+
+          </div>
+          <div className="flex-shrink-0">
+              <ScoreCircle score={feedback.overallScore} />
+              </div>
+            </div>
+      </Link>
   )
 }
 
