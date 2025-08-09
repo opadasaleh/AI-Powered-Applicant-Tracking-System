2669263e4cabcedd516dd9d4bef44ab9217d9a5a
opada <opadasadleh@gmail.com>
2025-08-09 18:04:05 +0300

    setup homaepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,17 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream jop!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg)] bg-cover">
+    <section className="main-section"></section>
+    <div className="page-heading">
+      <h1>Track Your Applecation & Resume Ratings</h1>
+      <h2>Review your submissions and check AI-powered feedback.</h2>
+    </div>
+  </main>
 }
