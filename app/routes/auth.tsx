46eb97fd0f39cc06abc5954d1d9c6695704507bb
opada <kami18102004@gmail.com>
2025-08-11 18:02:43 +0300

    build auth.tsx

---
+++ app/routes/auth.tsx
@@ -0,0 +1,15 @@
+import { title } from 'process'
+import React from 'react'
+
+export const meta = () => ([
+    { title: 'Resumind | Auth' },
+    {name: 'description ', content:'Log into your account '},
+])
+
+const auth = () => {
+  return (
+    <div>auth</div>
+  )
+}
+
+export default auth
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dns from 'node:dns'
+import { resolve } from "node:path";
+
+dns.setDefaultResultOrder('verbatim')
+
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias:[{find:"@" , replacement:resolve(__dirname,"./src")}]
+  }
 });
