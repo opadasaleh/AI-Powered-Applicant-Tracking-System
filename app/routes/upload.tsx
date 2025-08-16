c99b0dd18b408b48b289b0bdd9581279c737c6d7
opada <kami18102004@gmail.com>
2025-08-16 22:15:00 +0300

    make upload form without upload area

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,18 +1,19 @@
-import {useState} from 'react'
+import {useState, type FormEvent} from 'react'
 import Navbar from '~/components/Navbar'
 
 function upload() {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     
-    
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => { }
+
     return (
 <main className="bg-[url(/images/bg-main.svg)] bg-cover">
             <Navbar />
             
             
           <section className="main-section">
-              <div className="page-heading">
+              <div className="page-heading py-16">
                     <h1>Smart feedback for your dream jop </h1>
                     {isProcessing ? (
                         <>
@@ -21,8 +22,23 @@ function upload() {
                         </>
                     ) : (
                             <h2>Drop your resume for an ATS score and improvement tips</h2>
-                    )
-                }
+                    )}
+                    {!isProcessing && (
+                        <form id='upload-form' onSubmit={handleSubmit} className='flex flex-col gap-4 mt-'>
+                            <div className='form-div'>
+                                <label htmlFor='company-name'>Comapny Name</label>
+                                <input type='text' name='company-name' placeholder='Company Name' id='company-name'></input>
+                            </div>
+                            <div className='form-div'>
+                                <label htmlFor='jop-title'>Jop Title</label>
+                                <input type='text' name='jop-title' placeholder='Jop Title' id='jop-title'></input>
+                            </div>
+                            <div className='form-div'>
+                                <label htmlFor='jop-description'>Jop Description</label>
+                                <textarea rows={5} name='jop-description' placeholder='Jop Description' id='jop-description'></textarea>
+                            </div>
+                        </form>
+                    )}
               </div>
           </section>
 
