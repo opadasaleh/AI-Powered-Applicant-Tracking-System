b9d17b44486996b02ba052e87856c14e825a9935
opada <kami18102004@gmail.com>
2025-08-22 15:26:23 +0300

    write the dropzone usage

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -3,8 +3,23 @@ import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div className='w-full gradient-border'>FileUploader</div>
+    <div className='w-full gradient-border'>
+      <div {...getRootProps()}>      
+        <input {...getInputProps()} />      
+        {        
+          isDragActive ?          
+            <p>Drop the files here ...</p> :          
+            <p>Drag 'n' drop some files here, or click to select files</p>      
+        }
+    
+      </div>
+    </div>
   )
 }
 
